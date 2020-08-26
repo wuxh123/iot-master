0a732696261ec7b4411af4face8d288ab38b4ac9
Jason <jason@zgwit.com>
2020-08-26 11:13:52 +0800

    修改构建命令

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ call env.bat
 
 set GOOS=linux
 
-go build -o o2api cmd/main.go
+go build -o dtu-admin main.go
 
 set GOOS=windows
 
-go build -o o2api.exe cmd/main.go
+go build -o dtu-admin.exe main.go
