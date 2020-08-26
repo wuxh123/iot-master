ec38eb8859b26426c5d42d7e31b6e291e0bf62b0
Jason <jason@zgwit.com>
2020-08-26 11:28:17 +0800

    枯

--- dtu/link.go
+++ dtu/link.go
@@ -1,6 +1,18 @@
 package dtu
 
+import "sync"
+
 type Link interface {
 	Open() error
 	Close() error
+}
+
+var links *sync.Map
+
+func init() {
+	links = new(sync.Map)
+}
+
+func Links() *sync.Map {
+	return links
 }
