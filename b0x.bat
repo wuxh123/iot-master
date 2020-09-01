0842107e18ac3e8449e58789cb28b468916a1492
Jason <jason@zgwit.com>
2020-09-02 07:20:08 +0800

    前端使用fileb0x编译，打包至exe中，避免前端版本混乱

---
+++ b0x.bat
@@ -0,0 +1,4 @@
+
+:: go get -u github.com/UnnoTed/fileb0x
+
+fileb0x b0x.yaml
--- web/app.go
+++ web/app.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
@@ -9,6 +8,7 @@ import (
 	_ "github.com/zgwit/dtu-admin/docs"
 	"github.com/zgwit/dtu-admin/web/api"
 	"github.com/zgwit/dtu-admin/web/open"
+	wwwFiles "github.com/zgwit/dtu-admin/www"
 	"log"
 	"net/http"
 )
@@ -23,8 +23,7 @@ func Serve()  {
 	app := gin.Default()
 
 	//前端静态文件
-	app.Use(static.Serve("/", static.LocalFile("./www/", false)))
-
+	//app.Use(static.Serve("/", static.LocalFile("./www/", false)))
 
 	api.RegisterRoutes(app.Group("/api"))
 	open.RegisterRoutes(app.Group("/open"))
@@ -33,11 +32,19 @@ func Serve()  {
 	//Swagger文档，需要先执行swag init生成文档
 	app.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	//app.Use( ginSwagger.WrapHandler(wwwFiles.Handler))
+
 	//支持前端框架Angular的无“#”路由
 	//app.GET("/*any", func(c *gin.Context) {
 	app.Use(func(c *gin.Context) {
 		if c.Request.Method == http.MethodGet {
-			http.ServeFile(c.Writer, c.Request, "./www/index.html")
+			if c.Request.RequestURI == "/" {
+				c.Request.URL.Path = "index.html"
+			} else if _, err := wwwFiles.FS.Stat(wwwFiles.CTX, c.Request.RequestURI) ; err != nil {
+				c.Request.URL.Path = "index.html"
+			}
+			wwwFiles.Handler.ServeHTTP(c.Writer, c.Request)
+			//http.ServeFile(c.Writer, c.Request, "./www/index.html")
 		}
 	})
 
