688d1daac904051468d8482f3c6dfedfcd5fb6ee
Jason <jason@zgwit.com>
2020-09-08 11:56:28 +0800

    枯

--- model/plugin.go
+++ model/plugin.go
@@ -7,9 +7,6 @@ type Plugin struct {
 	Name      string    `json:"name" xorm:"varchar(64)"`
 	AppKey    string    `json:"app_key" xorm:"varchar(64)"`
 	AppSecret string    `json:"app_secret" xorm:"varchar(64)"`
-	Address   string    `json:"address" xorm:"varchar(128)"`
-	Path      string    `json:"path" xorm:"varchar(256)"`
-	Entry     string    `json:"entry" xorm:"varchar(256)"`
 	ExpireAt  time.Time `json:"expire_at"`
 	CreatedAt time.Time `json:"created_at" xorm:"created"`
 }
