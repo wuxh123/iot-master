7b6f4edce2705d666597a9c6a9a51b79206c6db1
Jason <jason@zgwit.com>
2020-09-17 11:01:12 +0800

    小改动

--- dbus/server.go
+++ dbus/server.go
@@ -2,15 +2,25 @@ package dbus
 
 import (
 	"git.zgwit.com/iot/beeq"
+	"git.zgwit.com/iot/beeq/packet"
+	"log"
 )
 
 var hive *beeq.Hive
 
 func Start(addr string) error {
 	hive = beeq.NewHive()
+	hive.OnConnect(func(connect *packet.Connect, bee *beeq.Bee) bool {
+		log.Println("connect", connect, bee)
+		return true
+	})
+	hive.OnPublish(func(publish *packet.Publish, bee *beeq.Bee) bool {
+		log.Println("publish", publish, bee)
+		return true
+	})
 	return hive.ListenAndServe(addr)
 }
 
-func Hive() *beeq.Hive  {
+func Hive() *beeq.Hive {
 	return hive
 }
