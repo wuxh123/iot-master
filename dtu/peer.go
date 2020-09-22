921c1f334d04c2f5fed2c30cacf7417e3cb7eb2f
Jason <jason@zgwit.com>
2020-09-22 08:10:19 +0800

    添加peer，做透传，未具体实现，拟websocket

---
+++ dtu/peer.go
@@ -0,0 +1,20 @@
+package dtu
+
+import "net"
+
+type Peer struct {
+	conn net.Conn
+	link *Link
+}
+
+func PeerAccept(conn net.Conn) {
+
+}
+
+func (p *Peer) Send(buf []byte) error {
+	return nil
+}
+
+func (p *Peer) Close(buf []byte) error {
+	return nil
+}
