a19a091a9a22eb1c295aa999f58e2c6db75b1450
Jason <jason@zgwit.com>
2020-09-24 08:00:56 +0800

    监听标签切换，加载相关页面（未具体实现） 监听路由，无对应标签，则需要新建标签（未实现）

--- portal/src/app/main/main.component.ts
+++ portal/src/app/main/main.component.ts
@@ -58,20 +58,46 @@ export class MainComponent implements OnInit {
   @ViewChild("container", {read: ViewContainerRef}) container: ViewContainerRef;
 
   constructor(private mqtt: MqttService, private router: Router, private resolver: ComponentFactoryResolver) {
-    router.events.subscribe((e:any) => {
-      console.log('router event', e)
-      if (this.container && this.container.createComponent
-        && e.snapshot && e.snapshot.component && e.snapshot.children.length==0) {
-
-        const factory = resolver.resolveComponentFactory(e.snapshot.component);
-        this.container.clear();
-        const ref = this.container.createComponent(factory);
-        //TODO 第一次，container还未准备好
-        //TODO 添加路由参数
-        //TODO call ref.destroy() in ngOnDestroy
-      }
+    router.events.subscribe((e: any) => {
+      //console.log('router event', e)
+      // if (this.container && this.container.createComponent
+      //   && e.snapshot && e.snapshot.component && e.snapshot.children.length == 0) {
+      //
+      //   const factory = resolver.resolveComponentFactory(e.snapshot.component);
+      //   this.container.clear();
+      //   const ref = this.container.createComponent(factory);
+      //   //TODO 第一次，container还未准备好
+      //   //TODO 添加路由参数
+      //   //TODO call ref.destroy() in ngOnDestroy
+      //
+      //
+      // }
     });
     //TODO unsubscribe
+
+  }
+
+  tabs = [];
+
+  onTabsChange(e): void {
+    //TODO 实现标签页
+    // 1、打开对应标签，如果未加载组件，则解析路由，导入组件，已加载则忽略
+    // 2、路由切换，没有自动打开对应标签，可能情况：
+    //  a、标签未创建或已经关闭，此时需要自动创建新标签，打开页面
+    //  b、无效路由
+    //
+
+    //e.index
+
+    // this.router.routerState.snapshot.root.firstChild.firstChild.firstChild...
+    let route: any = this.router.routerState.snapshot.root;
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    const factory = this.resolver.resolveComponentFactory(route.component);
+
+    this.container.clear();
+    const ref = this.container.createComponent(factory);
 
   }
 
