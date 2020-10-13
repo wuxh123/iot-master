1ae3004b1e13ff5cf8b4cb49b64ea8d03bc7d0f4
Jason <jason@zgwit.com>
2020-10-13 19:36:40 +0800

    类型定义

---
+++ models/tunnel.go
@@ -0,0 +1,28 @@
+package models
+
+import "time"
+
+type Tunnel struct {
+	Id            int64 `json:"id"`
+	ModelId       int64 `json:"model_id"`
+	ModelTunnelId int64 `json:"model_tunnel_id"`
+
+	Inactive bool `json:"inactive"` //系统启动倒置置false
+
+	Created time.Time `json:"created" xorm:"created"`
+	Updated time.Time `json:"updated" xorm:"updated"`
+}
+
+type Link struct {
+	Id            int64 `json:"id"`
+	TunnelId      int64 `json:"tunnel_id"`
+	ModelId       int64 `json:"model_id"` //冗余，可以通过联合查询
+	ModelTunnelId int64 `json:"model_tunnel_id"`
+
+	Serial string `json:"serial" xorm:"index"`
+
+	Inactive bool `json:"inactive"` //系统启动倒置置false
+
+	Created time.Time `json:"created" xorm:"created"`
+	Updated time.Time `json:"updated" xorm:"updated"`
+}
