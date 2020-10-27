b676f0eceb0d746d7c9dfee6bba4245eca9670c1
Jason <jason@zgwit.com>
2020-10-28 07:52:07 +0800

    改动，添加数据区域

--- models/element.go
+++ models/element.go
@@ -25,37 +25,39 @@ type Element struct {
 	Updated time.Time `json:"updated" storm:"updated"`
 }
 
-type ElementVariable struct {
+type ElementDataArea struct {
 	ID        int `json:"id"`
 	ElementId int `json:"element_id"`
 
-	Offset    uint16 `json:"offset"`
-	ReadCode  uint8  `json:"read_code"`
-	WriteCode uint8  `json:"write_code"`
+	Name    string `json:"name"`
+	Address uint16 `json:"address"`
+	Type    string `json:"type"`
+	//Modbus
+	// discrete 输入离散量（2读多个）
+	// coil 线圈（1读多个、5写单个、15写多个）
+	// input 输入寄存器（4读多个）
+	// hold 保持寄存器（3读多个、6写单个、16写多个，--23读写多个--）
 
-	Name  string `json:"name"`
-	Alias string `json:"alias"` //别名，用于编程
-	Type  string `json:"type"`
-	Unit  string `json:"unit"` //单位
+	Created time.Time `json:"created" storm:"created"`
+	Updated time.Time `json:"updated" storm:"updated"`
+}
+
+type ElementVariable struct {
+	ID         int `json:"id"`
+	ElementId  int `json:"element_id"`
+	DataAreaId int `json:"data_area_id"`
+
+	Offset uint16 `json:"offset"`
+	Name   string `json:"name"`
+	Alias  string `json:"alias"` //别名，用于编程
+	Type   string `json:"type"`
+	Unit   string `json:"unit"` //单位
 
 	Scale   float32 `json:"scale"`   //倍率，比如一般是 整数÷10，得到
 	Correct float32 `json:"correct"` //校准
 
 	Default  string `json:"default"`
 	Writable bool   `json:"writable"` //可写，用于输出（如开关）
-
-	Created time.Time `json:"created" storm:"created"`
-	Updated time.Time `json:"updated" storm:"updated"`
-}
-
-type ElementBatch struct {
-	ID        int `json:"id"`
-	ElementId int `json:"element_id"`
-
-	Type   string `json:"type"` //read write
-	Code   uint8  `json:"code"` //功能码 3,4
-	Offset uint16 `json:"offset"`
-	Size   uint16 `json:"size"`
 
 	Created time.Time `json:"created" storm:"created"`
 	Updated time.Time `json:"updated" storm:"updated"`
