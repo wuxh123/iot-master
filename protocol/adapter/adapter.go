be2475f1b51d2ad04afebc59e9550926ac9ae6fd
Jason <jason@zgwit.com>
2020-11-03 15:52:27 +0800

    小优化

--- protocol/adapter/adapter.go
+++ protocol/adapter/adapter.go
@@ -2,6 +2,7 @@ package adapter
 
 import (
 	"errors"
+	"git.zgwit.com/zgwit/iot-admin/base"
 	"sync"
 )
 
@@ -10,6 +11,8 @@ type CodeMap map[string]uint8
 type Adapter interface {
 	Name() string
 	Version() string
+
+	Attach(link base.Link)
 
 	Read(slave uint8, code uint8, offset uint16, size uint16) ([]byte, error)
 	Write(slave uint8, code uint8, offset uint16, buf []byte) error
