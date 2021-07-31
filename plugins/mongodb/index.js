a48eb7752302d6e98921a4388eacb634c6bb01b5
Jason <jason@zgwit.com>
2021-07-31 08:06:09 +0800

    将表格查询封装成接口，通用

--- plugins/mongodb/index.js
+++ plugins/mongodb/index.js
@@ -75,3 +75,30 @@ exports.DateToObjectId = function (time) {
 exports.collection = function (col) {
     return exports.db.collection(col);
 };
+
+exports.bodyToStages = function (body) {
+    const stages = [];
+    let $match = {};
+    if (body.keyword) {
+        $match.$or = [{name: {$regex: body.keyword}}, {address: body.keyword}, {port: body.keyword}]
+    }
+    if (body.filter) {
+        body.filter.filter(f => f.value.length > 0).forEach(f => {
+            $match[f.key] = f.value.length === 1 ? f.value[1] : {$in: f.value};
+        })
+    }
+    stages.push({$match});
+
+    if (body.sort) {
+        const $sort = {};
+        body.sort.forEach(s => {
+            $sort[s.key] = s.value === 'ascend' ? 1 : -1;
+        })
+        stages.push({$sort})
+    }
+    if (body.pageIndex && body.pageSize) {
+        stages.push({$skip: body.pageIndex * body.pageSize});
+        stages.push({$limit: body.pageSize});
+    }
+    return stages;
+}
--- routes/acceptor/list.js
+++ routes/acceptor/list.js
@@ -3,29 +3,7 @@ const mongo = require_plugin("mongodb");
 exports.post = async ctx => {
     const body = ctx.request.body;
 
-    const stages = [];
-    let $match = {};
-    if (body.keyword) {
-        $match.$or = [{name: {$regex: body.keyword}}, {address: body.keyword}, {port: body.keyword}]
-    }
-    if (body.filter) {
-        body.filter.filter(f => f.value.length > 0).forEach(f => {
-            $match[f.key] = f.value.length === 1 ? f.value[1] : {$in: f.value};
-        })
-    }
-    stages.push({$match});
-
-    if (body.sort) {
-        const $sort = {};
-        body.sort.forEach(s => {
-            $sort[s.key] = s.value === 'ascend' ? 1 : -1;
-        })
-        stages.push({$sort})
-    }
-    if (body.pageIndex && body.pageSize) {
-        stages.push({$skip: body.pageIndex * body.pageSize});
-        stages.push({$limit: body.pageSize});
-    }
+    const stages = mongo.bodyToStages(ctx.request.body);
 
     //ctx.request.body
     const res = await mongo.collection("acceptor").aggregate(stages).toArray();
