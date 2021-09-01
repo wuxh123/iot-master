581e3d926b794ec4909ccd82a10e77031b57dc3f
Jason <jason@zgwit.com>
2021-09-01 23:30:16 +0800

    日志优化

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("./lib/init");
 //避免异常错误导致程序退出
 process.on('uncaughtException', function (err) {
     //打印出错误
-    console.log(err);
+    console.error(err);
     //打印出错误的调用栈方便调试
-    console.log(err.stack);
+    //console.log(err.stack);
 });
